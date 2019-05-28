6e2f06ddc00533b7366640f3b8f9a84a146b9c66
mmejidana <mmejidana@BAIDEVLP103>
2019-05-28 08:18:08 +0800

    Change 'world' to 'meji'

--- Activity1/Program.cs
+++ Activity1/Program.cs
@@ -6,7 +6,7 @@ namespace Activity1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Mej!");
         }
     }
 }
